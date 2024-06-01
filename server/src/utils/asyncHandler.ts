9c05a8d185834b4b963e6c58eef3f281825d5103
Pranav1703 <pranavsai2005@gmail.com>
2024-06-01 20:14:28 +0530

    wth is happening

--- server/src/utils/asyncHandler.ts
+++ server/src/utils/asyncHandler.ts
@@ -2,7 +2,7 @@ import { RequestHandler, Request, Response, NextFunction } from 'express';
 
 const asyncHandler = (requestHandler: RequestHandler) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(new Error("Error at req handler",err)));
     };
 };
 
