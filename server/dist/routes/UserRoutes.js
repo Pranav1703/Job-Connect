a38c538ea9cd4c9b5f2547b87da9f68375f23630
Pranav1703 <pranavsai2005@gmail.com>
2024-07-21 19:55:43 +0530

    gitignore update

--- server/dist/routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signUp, login, logOut } from "../controllers/user.js";
-import { isAuthorized } from "../middleware/auth.js";
-const router = express.Router();
-router.post("/signup", signUp);
-router.post("/login", login);
-router.post("/logout", isAuthorized, logOut);
-export default router;
--- server/dist/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(new Error("Error at req handler", err)));
-    };
-};
-export { asyncHandler };
